6ad16fe42db31505b3e85a8721978e045518bd9d
Muhammad Raiyaan <83336994+muhammedraiyaan2@users.noreply.github.com>
2021-09-26 15:21:30 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# blog
-This is a amazing blog
+# Blog
+
+It's a very simple blog and very nice step by step blog that you can use for education purpose.
+
+[Open blog website](https://muhammedraiyaan2.github.io/blog)
+
+# About us
+
+If you want ot know about us then go to the website and there is section name about us in the header of the right side
+
+# Contact us
+
+If you want to contact us go to the website and go to the contact us section in header of the right side
---
+++ script.js
@@ -0,0 +1,68 @@
+function login() {
+    if (localStorage.getItem("Name") == null) {
+        let name = prompt("Enter your name")
+        localStorage.setItem("Name", name)
+    }
+    else if (localStorage.getItem("Name") == "") {
+        let name = prompt("Enter your name")
+        localStorage.setItem("Name", name)
+    }
+    else if (localStorage.getItem("Name") != "") {
+        alert("Already logged in")
+    }
+}
+if (localStorage.getItem("Name") == null) {
+    localStorage.setItem("Name", "")
+}
+function logout() {
+    localStorage.clear()
+    if (localStorage.getItem("Name") == null) {
+        localStorage.setItem("Name", "")
+    }
+}
+if (localStorage.getItem("Name") == null) {
+    localStorage.setItem("Name", "")
+}
+else if (localStorage.getItem("Name") == `null`) {
+    localStorage.setItem("Name", "")
+}
+function feedback() {
+    alert(`${localStorage.getItem("Name")} welcome to the feedback. If your star rating is 5 then enter 5 if you or 4 enter 4 or 3 enter 3 or 2 enter 2 or 1 enter 1`)
+    let feedback = prompt("Please enter your feedback")
+    if (feedback == 5) {
+        alert("Thank you very much sir/mam you like our blog 😊")
+        alert("Thank you for your feedback sir/mam 👍")
+    }
+    else if (feedback == 4) {
+        alert("Thank you sir/mam 😀")
+        alert("Thank you for your feedback sir/mam 👍")
+    }
+    else if (feedback == 3) {
+        alert("Ok sir/mam 😶")
+        alert("Thank you for your feedback sir/mam 👍")
+    }
+    else if (feedback == 2) {
+        alert("Sorry sir/mam we will improve the blog 😨")
+        alert("Thank you for your feedback sir/mam 👍")
+    }
+    else if (feedback == 1) {
+        alert("We are so sorry sir/mam we will do updates and we will improve the website 😣")
+        alert("Thank you for your feedback sir/mam 👍")
+    }
+    else {
+        alert("Sir/Mam can't understand please try again 😅")
+    }
+}
+let i = 0
+let txt = `${localStorage.getItem("Name")} Welcome to the blog`
+let speed = 50
+let txt2 = `Blogs`
+function typeWriter() {
+    setInterval(() => {
+        if (i < txt.length) {
+            document.getElementById(`typings`).innerHTML += txt.charAt(i)
+            document.getElementById(`blogs`).innerHTML += txt2.charAt(i)
+            i++
+        }
+    }, speed);
+}
