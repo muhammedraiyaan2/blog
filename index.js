feb396ec1b01600ce1b38d3480ff445600bfe7c8
Muhammad Raiyaan <83336994+muhammedraiyaan2@users.noreply.github.com>
2021-10-09 11:06:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const index=()=>{
+    window.location.pathname="blog/index.html"
+}
+const about=()=>{
+    window.location.pathname="blog/about.html"
+}
+function contact(){
+    window.location.pathname="blog/contact.html"
+}
